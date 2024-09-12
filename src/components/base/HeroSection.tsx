a8ae5a2e1d6592d969bbafdac3326d9beb1adf7e
NehalSurti <nehalsurti9998@gmail.com>
2024-09-12 23:55:32 +0100

    Updating

--- src/components/base/HeroSection.tsx
+++ src/components/base/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function HeroSection() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-screen flex justify-center items-center flex-col">
       <div>
         <Image
           src="/banner_img.svg"
@@ -11,6 +11,12 @@ export default function HeroSection() {
           width={600}
           height={600}
         ></Image>
+        <div>
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold text-center bg-gradient-to-r from-pink-400 to-purple-500 text-transparent bg-clip-text">
+            Clash
+          </h1>
+          <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-center">Discover the better choice, together</p>
+        </div>
       </div>
     </div>
   );
